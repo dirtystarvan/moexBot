0cb10370794e2e220d0a803de6d5261f9505a825
eugenekorolev <100HzJugin@gmail.com>
2021-08-27 19:21:43 +0300

    init commit

---
+++ src/test/java/ru/sbrf/trade/data/legacy/datacollector/DataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sbrf.trade.data.legacy.datacollector;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+class DataCollectorApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
